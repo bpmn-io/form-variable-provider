9ccf21b527d3a1f4aaa758ed14f83ab3917e8bce
Martin Stamm <martin.stamm@camunda.com>
2023-08-18 11:35:22 +0200

    feat: add form variable extractor

--- test/TestHelper.js
+++ test/TestHelper.js
@@ -39,6 +39,42 @@ global.chai.use(function(chai, utils) {
       true // show diff
     );
   });
+
+  utils.addMethod(chai.Assertion.prototype, 'variableEqual', function(comparison) {
+    var variables = this._obj;
+    var expectedVariables = comparison;
+
+    expect(variables.length).to.eql(expectedVariables.length);
+
+    expectedVariables.forEach((expectedVariable) => {
+      const {
+        name,
+        type,
+        detail,
+        info,
+        scope,
+        isList,
+        origin,
+        entries
+      } = expectedVariable;
+
+      const actualVariable = variables.find(v => v.name === name);
+      expect(actualVariable).to.exist;
+
+      isDefined(type) && expect(actualVariable.type).to.eql(type);
+      isDefined(info) && expect(actualVariable.info).to.eql(info);
+      isDefined(detail) && expect(actualVariable.detail).to.eql(detail);
+      isDefined(scope) && expect(actualVariable.scope.id).to.eql(scope);
+      isDefined(isList) && expect(!!actualVariable.isList).to.eql(!!isList);
+      isDefined(entries) && expect(actualVariable.entries).to.variableEqual(entries);
+
+      isDefined(origin) && expect(actualVariable.origin.length).to.eql(origin.length);
+      isDefined(origin) && origin.forEach((expectedOrigin) => {
+        const foundOrigin = actualVariable.origin.find(o => o.id === expectedOrigin);
+        expect(foundOrigin).to.exist;
+      });
+    });
+  });
 });
 
 export * from 'bpmn-js/test/helper';
@@ -221,3 +257,8 @@ document.addEventListener('keydown', function(event) {
     download(result.xml, 'test.bpmn', 'application/xml');
   });
 });
+
+
+function isDefined(value) {
+  return typeof value !== 'undefined';
+}
