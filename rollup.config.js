6cecb58b0996b8195e54326b662a85ec326f5597
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2023-12-15 09:48:49 +0100

    chore: prevent bundle warning
    Correctly mark peer dependencies as external.

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,26 @@ const commonjs = require('@rollup/plugin-commonjs');
 const json = require('@rollup/plugin-json');
 
 const pkg = require('./package.json');
-const nonbundledDependencies = Object.keys({ ...pkg.dependencies });
+const nonbundledDependencies = [
+  ...Object.keys({
+    ...pkg.dependencies,
+    ...pkg.peerDependencies
+  }),
+  'bpmn-js/lib/util/ModelUtil'
+];
 
 module.exports = {
   input: 'lib/index.js',
-  output: [ {
-    file: pkg.main,
-    format: 'cjs'
-  },
-  {
-    file: pkg.module,
-    format: 'esm'
-  } ],
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    }
+  ],
   plugins: [
     commonjs(),
     json()
